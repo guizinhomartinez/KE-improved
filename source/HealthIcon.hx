b626cf6c2fe24dc69a7df60fb6e69d21278c6e5a
guizinhomartinez <guilhermemartinez39@gmail.com>
2021-06-01 14:11:58 -0300

    Initial commit

---
+++ source/HealthIcon.hx
@@ -0,0 +1,58 @@
+package;
+
+import flixel.FlxSprite;
+
+class HealthIcon extends FlxSprite
+{
+	/**
+	 * Used for FreeplayState! If you use it elsewhere, prob gonna annoying
+	 */
+	public var sprTracker:FlxSprite;
+
+	public function new(char:String = 'bf', isPlayer:Bool = false)
+	{
+		super();
+		
+		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
+
+		antialiasing = true;
+		animation.add('bf', [0, 1, 24], 0, false, isPlayer);
+		animation.add('bf-car', [0, 1, 24], 0, false, isPlayer);
+		animation.add('bf-christmas', [0, 1, 24], 0, false, isPlayer);
+		animation.add('bf-pixel', [21, 21, 25], 0, false, isPlayer);
+		animation.add('spooky', [2, 3, 2], 0, false, isPlayer);
+		animation.add('pico', [4, 5, 32], 0, false, isPlayer);
+		animation.add('mom', [6, 7, 6], 0, false, isPlayer);
+		animation.add('mom-car', [6, 7, 6], 0, false, isPlayer);
+		animation.add('tankman', [8, 9, 8], 0, false, isPlayer);
+		animation.add('face', [10, 11, 10], 0, false, isPlayer);
+		animation.add('dad', [12, 13, 12], 0, false, isPlayer);
+		animation.add('senpai', [22, 30, 31], 0, false, isPlayer);
+		animation.add('senpai-angry', [28, 27, 29], 0, false, isPlayer);
+		animation.add('spirit', [23, 23, 23], 0, false, isPlayer);
+		animation.add('bf-old', [14, 15, 26], 0, false, isPlayer);
+		animation.add('gf', [16, 16, 16], 0, false, isPlayer);
+		animation.add('gf-christmas', [16, 16, 16], 0, false, isPlayer);
+		animation.add('gf-pixel', [16, 16, 16], 0, false, isPlayer);
+		animation.add('parents-christmas', [17, 18, 17], 0, false, isPlayer);
+		animation.add('monster', [19, 20, 19], 0, false, isPlayer);
+		animation.add('monster-christmas', [19, 20, 19], 0, false, isPlayer);
+		animation.play(char);
+
+		switch(char)
+		{
+			case 'bf-pixel' | 'senpai' | 'senpai-angry' | 'spirit' | 'gf-pixel':
+				antialiasing = false;
+		}
+
+		scrollFactor.set();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (sprTracker != null)
+			setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
+	}
+}
